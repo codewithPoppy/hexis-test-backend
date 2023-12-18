35c08efa3642a78389d0d1c836ee0d80b31cea20
codewithPoppy <nomatter2challenge@outlook.com>
2023-12-18 09:46:53 +0000

    feat: add cors config

--- index.ts
+++ index.ts
@@ -4,13 +4,30 @@ dotenv.config();
 import { ApolloServer } from "apollo-server-micro";
 import typeDefs from "./schema";
 import resolvers from "./resolvers";
+import Cors from "micro-cors";
+
+const cors = Cors({
+  allowMethods: ["GET", "POST", "OPTIONS"],
+  allowHeaders: ["Content-Type", "Authorization"],
+  origin: "*",
+});
 
 const startServer = async () => {
-  const server = new ApolloServer({ typeDefs, resolvers });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
 
   await server.start();
 
-  return server.createHandler({ path: "/" });
+  const handler = server.createHandler({ path: "/" });
+  return cors((req, res) => {
+    if (req.method === "OPTIONS") {
+      res.end();
+      return;
+    }
+    return handler(req, res);
+  });
 };
 
 export default startServer();
